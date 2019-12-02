3c91d8d9e923754548fe4fbf85c41fe0ec847ee1
gamaroid <52582680+gamaroid@users.noreply.github.com>
2019-12-01 21:05:56 -0300

    to do list

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Estudando JavaScript</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+</head>
+<body>
+ <section >
+
+ <div class="container">
+  <div class="row">
+    <div class="col-sm list-group div-1">
+    <a  id="color" class="list-group-item list-group-item-action "><h1 class="text-p">links rapidos para agilizar o dia</h1></a>
+              
+                
+               <a href="https://skylab.rocketseat.com.br/node/curso-java-script/group/introducao-java-script/lesson/introducao-2" target="_blanck" id="color" class="list-group-item list-group-item-action list-group-item-info">Skylab Rockseat</a><a href="http://distancia.qi.edu.br/my/"  target="_blanck" class="list-group-item list-group-item-action list-group-item-primary " id="color" >Qi Faculdade</a>
+              <a href="https://www.youtube.com/"target="_blanck" id="color"  class="list-group-item list-group-item-action list-group-item-secondary">YouTube</a>
+              <a href="https://www.google.com/" target="_blanck"id="color" class="list-group-item list-group-item-action list-group-item-success">Google</a>
+              <a href="https://qi.edu.br/" target="_blanck" id="color" class="list-group-item list-group-item-action list-group-item-danger">Qi Site</a>
+              <a href="https://app-qifly.qi.edu.br/guide/recommend/goal/5d8e279dd26b137b8d1f4398/" target="_blanck" id="color" class="list-group-item list-group-item-action list-group-item-warning">Curso De Ingles</a>
+             
+              <!-- <a href="#" target="_blanck" id="color" class="list-group-item list-group-item-action list-group-item-light">A simple light list group item</a>
+              <a href="#" target="_blanck" id="color"class="list-group-item list-group-item-action list-group-item-dark">A simple dark list group item</a> -->
+    </div >
+    <div class="col-sm list-group div-1">
+    </div>
+    <div class="col-sm">
+     
+    </div>
+  </div>
+</div>
+        <div class="list-group div-1 container">
+                
+              </div>
+</section>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
